d341248bbf9f8896ce1d7d0fe5901d525158278e
ArghRUS <tolstovsm@mail.ru>
2020-01-28 09:17:36 +0300

    Promises

--- js/js.js
+++ js/js.js
@@ -1,39 +1,33 @@
-// let options={
-//     width:1366,
-//     height:768,
-//     background: 'red',
-//     font:{
-//         size:'16px',
-//         color:'#fff'
-//     } 
-// };
-
-// console.log(JSON.parse(JSON.stringify(options)));
 let inputRub=document.getElementById('rub'),
     inputUSD=document.getElementById('usd');
 
 inputRub.addEventListener('input',()=>{
-    let request=new XMLHttpRequest();
+    function getData(){
+        return new Promise(function(resolve, reject){
+            let request=new XMLHttpRequest();
+            request.open('GET', 'js/current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+            request.onreadystatechange=function(){
+                if (request.readyState===4){
+                    if (request.status==200){
+                        resolve(this.response);
+                    } else {
+                        reject();
+                    }
+                } 
+                    
+            };
 
-    // request.open(method, url, async, login, pass);
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    // request.send(body)
-    request.send();
+        });
+    }
 
-    //status
-    //statusText
-    //responseText / response
-    //readyState
-
-    request.addEventListener('readystatechange',function(){
-        if (request.readyState===4 && request.status==200){
-            let data=JSON.parse(request.response);
-
+    getData()
+        .then(response=>{
+            // console.log(response);
+            let data=JSON.parse(response);
             inputUSD.value=inputRub.value/data.usd;
-        } else{
-            inputUSD.value="Что-тоо пошло не так!";
-        }
-    })
+        })
 
+        .catch(()=>inputUSD.value="Что-то пошло не так!");
 });
